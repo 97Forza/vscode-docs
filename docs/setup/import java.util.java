cb89b507dd8f38986b3c9c9b5dc6b05e733bd779
97Forza <ardakapcak1792@gmail.com>
2024-12-21 22:53:43 +0300

    arda

---
+++ docs/setup/import java.util.java
@@ -0,0 +1,7 @@
+import java.util.scanner ;
+public class Cember {
+    public static void main(String[]  args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print('Çemberin yarı');
+    }
+}
